91def89ed7db4d82a96d93cf8e881b31a937b5d4
fiorentinoing <stefano.fiore84@gmail.com>
2017-11-15 23:07:33 +0100

    mutex lock/unlock example, managed by Lock class (the point here isn't to show how std::mutex works). example of why using resource-managing object prevent resource leak.

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <iostream>
+
+#include "include/Lock.h"
+
+namespace
+{
+    void solution1()
+    {
+        std::string* ps = new std::string;
+        mutex_lock::Lock ml(&mutex_lock::mutex);
+        ps->insert(0, "ciao!");
+
+        // here is an exception-raising function call
+        throw std::runtime_error("prevent to printout and clean the heap");
+
+        delete ps; // because the runtime never reach this point, we have resource leak
+    }
+
+    void solution2()
+    {
+        std::shared_ptr<std::string> ps = std::shared_ptr<std::string>(new std::string);
+        mutex_lock::Lock ml(&mutex_lock::mutex);
+        ps->insert(0, "ciao!");
+
+        // here is an exception-raising function call
+        throw std::runtime_error("prevent to printout and clean the heap");
+
+        // because of the resource-manager object `shared_ptr`,
+        // no need to delete the string pointed by ps.
+    }
+}
+
+int main()
+{
+    try
+    {
+        ::solution1();
+    }
+    catch(std::runtime_error const& exp)
+    {
+        std::cout << exp.what() << std::endl;
+    }
+
+    try
+    {
+        ::solution2();
+    }
+    catch(std::runtime_error const& exp)
+    {
+        std::cout << exp.what() << std::endl;
+    }
+    return 0;
+}
